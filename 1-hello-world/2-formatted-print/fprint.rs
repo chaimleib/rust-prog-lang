6e61f2d3205acd619690b956a78206667105771e
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2017-11-13 11:56:41 -0800

    1-2 complete fixmes

--- 1-hello-world/2-formatted-print/fprint.rs
+++ 1-hello-world/2-formatted-print/fprint.rs
@@ -13,6 +13,13 @@ fn main() {
     println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
     println!("{number:>width$}", number=1, width=6);
     println!("{number:>0width$}", number=1, width=6);
+    
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    
+    //#[allow(dead_code)]
+    #[derive(Debug)]
+    struct Structure(i32);
+    println!("This struct `{:?}` won't print...", Structure(3));
 }
 
 fn introduce(a: &str, b: &str) -> () {
