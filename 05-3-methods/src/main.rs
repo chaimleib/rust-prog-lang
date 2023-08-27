33553130bd4e7be834bdd361efdaacd8c3496b30
Chaim Halbert <chaim.leib.halbert@gmail.com>
2023-08-27 01:59:28 -0700

    05-3-methods: convert area() to method, add can_hold(other) and square(size)

--- 05-3-methods/src/main.rs
+++ 05-3-methods/src/main.rs
@@ -4,17 +4,36 @@ struct Rectangle {
     height: u32,
 }
 
-fn area(r: &Rectangle) -> u32 {
-    r.width * r.height
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Self) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
-    let scale = 2;
-    let r = Rectangle {
-        width: dbg!(30 * scale),
+    let r1 = Rectangle {
+        width: 30,
         height: 50,
     };
+    let r2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let r3 = Rectangle::square(60);
 
-    println!("r: {:#?}", r);
-    println!("area: {}", area(&r));
+    dbg!(r1.can_hold(&r2));
+    dbg!(r2.can_hold(&r3));
+    dbg!(r1.can_hold(&r3));
+    dbg!(r3.area());
 }
