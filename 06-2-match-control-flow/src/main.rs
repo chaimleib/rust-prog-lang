a7f7369b117c3b6944bfb56c30a23ac5ca5ee90f
Chaim Halbert <chaim.leib.halbert@gmail.com>
2023-09-24 15:53:16 -0700

    6-2 enum Coin

--- 06-2-match-control-flow/src/main.rs
+++ 06-2-match-control-flow/src/main.rs
@@ -1,3 +1,21 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+impl Coin {
+    fn value(self) -> u8 {
+        match self {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+}
+
 fn plus_one_match(x: Option<i32>) -> Option<i32> {
     match x {
         Some(x) => Some(x+1),
@@ -8,6 +26,8 @@ fn plus_one_match(x: Option<i32>) -> Option<i32> {
 fn plus_one(x: Option<i32>) -> Option<i32> { x.map(|y| y + 1) }
 
 fn main() {
+    dbg!(Coin::Dime.value());
+
     dbg!(plus_one(Some(5)));
     dbg!(plus_one(None));
     dbg!(plus_one_match(Some(5)));
