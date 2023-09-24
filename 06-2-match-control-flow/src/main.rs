673f38643474c409ecd7c838753ff449fea80266
Chaim Halbert <chaim.leib.halbert@gmail.com>
2023-09-24 16:07:42 -0700

    6-2 Coin::Quarter(UsState), plus note about concurrent dbg! and println!

--- 06-2-match-control-flow/src/main.rs
+++ 06-2-match-control-flow/src/main.rs
@@ -1,8 +1,17 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // etc
+}
+
+#[allow(dead_code)]
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 impl Coin {
@@ -11,7 +20,10 @@ impl Coin {
             Coin::Penny => 1,
             Coin::Nickel => 5,
             Coin::Dime => 10,
-            Coin::Quarter => 25,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
         }
     }
 }
@@ -25,11 +37,20 @@ fn plus_one_match(x: Option<i32>) -> Option<i32> {
 
 fn plus_one(x: Option<i32>) -> Option<i32> { x.map(|y| y + 1) }
 
-fn main() {
-    dbg!(Coin::Dime.value());
-
+fn plus_one_tests() {
+    // dbg! and println! statements seem to be running concurrently, and so output ordering on
+    // stdout and stderr are individually stable, but may interleave nondeterministically.
+    println!("## Plus one tests");
     dbg!(plus_one(Some(5)));
     dbg!(plus_one(None));
     dbg!(plus_one_match(Some(5)));
     dbg!(plus_one_match(None));
+}
+
+fn main() {
+    plus_one_tests();
+
+    println!("## Coin tests");
+    dbg!(Coin::Dime.value());
+    dbg!(Coin::Quarter(UsState::Alabama).value());
 }
